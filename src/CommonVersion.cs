61eb806789dbd3b16b9a74ce0ef0f4c7de592ca9
Sergej Derjabkin <sergej@sendev.de>
2019-06-25 22:36:45 +0200

    - Added CommonVersion.cs

---
+++ src/CommonVersion.cs
@@ -0,0 +1,1 @@
+[assembly: System.Reflection.AssemblyVersion("19.1.4.1")]
--- src/SenDev.Xaf.HoursPropertyEditor.Web/Properties/AssemblyInfo.cs
+++ src/SenDev.Xaf.HoursPropertyEditor.Web/Properties/AssemblyInfo.cs
@@ -21,18 +21,5 @@ using System.Web.UI;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: WebResource(HoursSpinEdit.ScriptResourceName, "text/javascript")]
 [assembly: WebResourceAssembly(2349)]
